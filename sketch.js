933bbd4477f12d2481e9db310a30f7d6d997d0fd
ashishthakur100 <77110322+ashishthakur100@users.noreply.github.com>
2021-01-19 15:56:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var fixedRect, movingRect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(300,200,50,80);
+  movingRect=createSprite(400,200,80,30);
+
+  fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+
+
+}
+
+function draw() {
+  background(0); 
+  console.log(movingRect.x-fixedRect.x)
+movingRect.x=World.mouseX;
+movingRect.y=World.mouseY;
+
+if(movingRect.x-fixedRect.x< fixedRect.width/2+movingRect.width/2
+&& 
+fixedRect.x-movingRect.x< fixedRect.width/2+movingRect.width/2 
+  &&
+  movingRect.y-fixedRect.y< fixedRect.height/2+movingRect.height/2
+&& 
+fixedRect.y-movingRect.y< fixedRect.height/2+movingRect.height/2 
+  
+   )
+{
+
+  fixedRect.shapeColor="white";
+  movingRect.shapeColor="white";
+
+}
+else
+{
+  fixedRect.shapeColor="green";
+  movingRect.shapeColor="green";
+
+}
+  drawSprites();
+}
